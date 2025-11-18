4f52ca53c1e70c389558bf15a021611ff7cb4dba
Akhil Chaudhary <chaudharyakhil0511@gmail.com>
2025-11-18 20:35:25 +0530

    Updating ATM class project

--- ATM.py
+++ ATM.py
@@ -1,18 +1,30 @@
 class Atm:
 
     def __init__(self):     # Constructor
-        self.pin = ""
-        self.balance = 0
-        self.menu()        # Calling the Menu() method for the object
+        self.__pin = ""         # Private variable
+        self.__balance = 0      #    "      "
+        self.__menu()           #    "      "
 
-    def menu(self):
+    def get_pin(self):                  # Getter Method
+        print("Pin =",self.__pin)
+
+    def set_pin(self,new_pin):          # Setter Method
+        if type(new_pin) is str:
+            self.__pin = new_pin
+            print("Pin set successfully.")
+        else:
+            print("Invalid format.")
+
+    def __menu(self):       # Private method
         while True:
             user_input = input('''Hello, how will you like to proceed?
                                     1. Create Pin
                                     2. Deposit
                                     3. Withdraw
                                     4. Check Balance
-                                    5. Exit\n''')
+                                    5. Get Pin
+                                    6. Set Pin
+                                    7. Exit\n''')
             if user_input == "1":
                 self.create_pin()
             elif user_input == "2":
@@ -22,6 +34,11 @@ class Atm:
             elif user_input == "4":
                 self.check_balance()
             elif user_input == "5":
+                self.get_pin()
+            elif user_input == "6":
+                new_pin = input("Enter new pin : ")
+                self.set_pin(new_pin)
+            elif user_input == "7":
                 print("Exiting...")
                 break
             else:
@@ -29,24 +46,24 @@ class Atm:
 
 
     def create_pin(self):
-        self.pin = input("Enter your pin : ")
+        self.__pin = input("Enter your pin : ")
         print("Pin set successfully.")
 
     def deposit(self):
         temp = input("Enter the pin : ")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter the amount : "))
-            self.balance += amount
+            self.__balance += amount
             print("Deposited",amount,"successfully.")
         else:
             print("Invalid Pin.")
 
     def withdraw(self):
         temp = input("Enter the pin : ")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter the amount : "))
-            if amount <= self.balance:
-                self.balance -= amount
+            if amount <= self.__balance:
+                self.__balance -= amount
                 print("Withdrawal successful.")
             else:
                 print("Insufficient Balance.")
@@ -55,8 +72,8 @@ class Atm:
 
     def check_balance(self):
         temp = input("Enter the pin : ")
-        if temp == self.pin:
-            print(self.balance)
+        if temp == self.__pin:
+            print(self.__balance)
         else:
             print("Invalid pin.")
 
